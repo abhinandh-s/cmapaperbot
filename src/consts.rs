adf0b27c4c38833f226e7dffdf3a71c779aa7047
Abhinandh S <abhinandhsuby@proton.me>
2026-08-05 21:56:23 +0530

    Update consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,8 @@ use wasm_bindgen::prelude::*;
 pub fn privacy_policy() -> String {
     include_str!("../docs/privacy.md").into()
 }
+
+#[wasm_bindgen]
+pub fn support() -> String {
+    include_str!("../docs/support.md").into()
+}
