2e28afac77d8c51da79b02867c96624a04334c81
abhinandh-s <168844210+abhinandh-s@users.noreply.github.com>
2026-08-05 05:14:29 +0000

    chore: compile rust to wasm

--- lib/cmapaperbot.d.ts
+++ lib/cmapaperbot.d.ts
@@ -12,6 +12,8 @@ export function level_in_blockquotes(id: string): string;
 
 export function level_of(id: string): string;
 
+export function privacy_policy(): string;
+
 export function renderSet(id: string): string;
 
 export function renderSyllabus(syl: string): string;
