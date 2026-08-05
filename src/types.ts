33a80cee84e7790f43e39c20303e879db85a70c7
Abhinandh S <abhinandhsuby@proton.me>
2026-08-05 13:03:55 +0530

    Delete types.ts

--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export type Level = "foundation" | "intermediate" | "final";
-export type DocType = "pyq" | "mqp" | "ptp";
-
-export type FileRecord = { name: string; id: string; syllabus?: string }[];
